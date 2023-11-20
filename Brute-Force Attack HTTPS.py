3dd95c1ee3ac9cac596587361b17ce8c4c46a053
ruhanslater2002 <78014411+ruhanslater2002@users.noreply.github.com>
2023-11-20 16:31:02 +0200

    Update Brute-Force Attack HTTPS.py

--- Brute-Force Attack HTTPS.py
+++ Brute-Force Attack HTTPS.py
@@ -3,14 +3,14 @@ import sys
 
 try:
     #sys.argv is the placeholder for the inputs
-    filePath = sys.argv[1]
-    username = sys.argv[2]
-    lastPassword = sys.argv[3]
-    url = sys.argv[4]
-    cUsername = str(sys.argv[5])
-    cPassword = str(sys.argv[6])
-    cLogin = str(sys.argv[7])
-    failsMsg = str(sys.argv[8])
+    filePath = sys.argv[1] #File path way
+    username = sys.argv[2] #Login username
+    lastPassword = sys.argv[3] #This if you know the last parts of the password for example {wordlist}{lastPassword}
+    url = sys.argv[4] #This is the url page that the login should be brute-forced
+    cUsername = str(sys.argv[5]) #This is the username credential variable on the html page
+    cPassword = str(sys.argv[6]) #This is the password credential variable on the html page
+    cLogin = str(sys.argv[7]) #This is the login button credential variable on the html page
+    failsMsg = str(sys.argv[8]) #This is the message that will popup if the password is incorrect
 
     if lastPassword == "-n":
         lastPassword = ""
